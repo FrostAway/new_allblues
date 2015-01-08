4c19c869bd734a85e64f878f946edefdf952d988
FrostLife07 <skyfrost.07@gmail.com>
2015-01-08 22:48:29 +0700

    breadcrum

--- wp-content/themes/allblue_theme/inc/breadcrum.php
+++ wp-content/themes/allblue_theme/inc/breadcrum.php
@@ -2,42 +2,27 @@
 <div class="breadcrumb full">
     <div class="wrapper">
         <div class="left-bread">
-                       
-            <?php if (have_posts()) : ?>
-
- 			<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-
-			<?php /* If this is a category archive */ if (is_category()) { ?>
-				<h1><?php single_cat_title(); ?></h1>
-                                
-			<?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-				<h1><?php single_tag_title(); ?></h1>
-
-			<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-				<h1><?php the_time('F jS, Y'); ?></h1>
-
-			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-				<h1><?php the_time('F, Y'); ?></h1>
-
-			<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-				<h1><?php the_time('Y'); ?></h1>
-
-			<?php /* If this is an author archive */ } elseif (is_author()) { ?>
-				<h1>Author Archive</h1>
-
-			<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-				<h1>Blog Archives</h1>
-			
-			<?php } ?>
-                                
-                                
-                   <?php else : ?>
-
-		<h2>Nothing found</h2>
-
-	<?php endif; ?>
+            <h1>            
+           <?php
+		      if (function_exists('is_tag') && is_tag()) {
+		        single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+		      elseif (is_archive()) {
+		         wp_title(''); echo ' Archive - '; }
+		      elseif (is_search()) {
+		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+		      elseif (!(is_404()) && (is_single()) || (is_page())) {
+		         wp_title(''); echo ' - '; }
+		      elseif (is_404()) {
+		         echo 'Not Found - '; }
+		      if (is_home()) {
+		         bloginfo('name'); echo ' - '; bloginfo('description'); }
+		      else {
+		          bloginfo('name'); }
+		      if ($paged>1) {
+		         echo ' - page '. $paged; }
+		   ?>
             
-            
+            </h1>
         </div>
         <div class="right-bread">
             <?php the_breadcrumb(); ?>
